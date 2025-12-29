a0e71a5f53e434dd39b74e300f503a6a011de0c6
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2025-12-29 20:21:56 +0100

    generate_changes.py: Make deleted commit a link.

--- generate_changes.py
+++ generate_changes.py
@@ -743,7 +743,13 @@ def generate_repo_branch_changes(markdown_output, repo_config, repo_categories, 
 
     for branch in removed_branches:
         parent_commit = parent_branches[branch].get("latest_commit", "unknown")
-        markdown_output += f"- **{branch}** (was `{parent_commit}`)\n"
+
+        cfg = repo_config.get(repo_id, {})
+        base = cfg.get("base", f"https://github.com/Zimbra/{repo_id}")
+        platform = cfg.get("platform", "github")
+        links = make_repo_links(base, platform, repo_id, branch, type="commit", commit_hash=parent_commit)
+
+        markdown_output += f"- **{branch}** (was [{parent_commit}]({links['commit']}))\n"
 
     markdown_output += "\n"
     return markdown_output
